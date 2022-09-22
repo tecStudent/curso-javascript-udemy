32a123700ebe59d2fdbef342872c80ebec896c18
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-09-21 22:31:20 -0300

    correcao do exercicio

--- secao1/8.calculadora/assets/js/main.js
+++ secao1/8.calculadora/assets/js/main.js
@@ -1,33 +1,68 @@
-const inputCalculo = document.querySelector('.input-valores')
+function criarCalculadora(){
+    return {
+        display: document.querySelector('.input-valores'),
 
 
-document.addEventListener('click', (e)=>{
-    const evento = e.target
-    insereValoresNoInput(evento.innerText)
-    if(evento.innerText === 'C'){
-        limpaTudoNoInput()
+        inicia(){
+            this.cliqueBotoes()
+            this.botaoEnter()
+        },
+        limpaDisplay(){
+            this.display.value = ''
+        },
+        apagaDigito(){
+            this.display.value = this.display.value.slice(0,-1)
+        },
+        realizaConta(){
+            let conta = this.display.value
+            try{
+                conta = eval(conta)
+                if(!conta){
+                    alert('Conta inválida')
+                    return
+                }
+                this.display.value = String(conta)
+            }catch(e){
+                if(!conta){
+                    alert('Conta inválida')
+                    return
+                }
+
+            }
+        },
+        botaoEnter(){
+            document.addEventListener('keyup', (e)=>{
+                if(e.keyCode === 13){
+                    this.realizaConta()
+                }
+            })
+        },
+
+        cliqueBotoes(){
+            document.addEventListener('click', (e) =>{
+                const el = e.target
+                
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText)
+                
+                }
+                if(el.classList.contains('c')){
+                    this.limpaDisplay()   
+                }
+                if(el.classList.contains('apagar-digito')){
+                    this.apagaDigito()
+                }
+                if(el.classList.contains('igual')){
+                    this.realizaConta()
+                }
+                this.display.focus()
+            })
+        },
+        btnParaDisplay(valor){
+            this.display.value += valor
+        }
     }
-    if(evento.classList.contains('apagar-digito')){
-        limpaUltimoDigitoInput(3)
-    }
-    if(evento.classList.contains('igual')){
-        limpaUltimoDigitoInput(1)
-        const conta = Number(inputCalculo.value) * 0
-        console.log(conta)
-    }
-
-})
-
-
-function insereValoresNoInput(texto){
-    inputCalculo.value += texto
 }
-
-function limpaTudoNoInput(){
-    inputCalculo.value = ''
-}
-function limpaUltimoDigitoInput(numero){
-    const total = inputCalculo.value
-    inputCalculo.value = total.substring(0,total.length-numero)
-    
-}
+const calculadora = criarCalculadora()
+calculadora.inicia()
